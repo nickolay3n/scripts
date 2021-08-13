48d6e946b157ee4ef82fd0dbab50e04b9dd230c8
Николай <nickolay3n@gmail.com>
2021-08-13 13:15:42 +0300

    unzip kave.zip.cmd for windows

---
+++ kav/unzip kave.zip.cmd
@@ -0,0 +1,32 @@
+rem @echo off
+
+set dd=%DATE%
+
+set /a ddd=%dd:~0,2%
+set /a ddd-=1
+IF %ddd% LSS 10 (
+  SET day=0%ddd%) else (
+  SET day=%ddd%)
+set month=%dd:~3,2%
+set year=%dd:~6,4%
+
+copy \\IP\kav\kave-%year%-%month%-%day%.zip  c:\kav\kave.zip
+
+
+Call :UnZipFile "C:\kavupdates\" "c:\kav\kave.zip"
+exit /b
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
